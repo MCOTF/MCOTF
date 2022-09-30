d5f18759fe31d702fed2985ef968c299e1261c56
二三三的石头 <mcotf@outlook.com>
2022-09-30 02:46:28 +0000

    对接到idea上

---
+++ Java课程记录/Java堆栈特性1.java
@@ -0,0 +1,10 @@
+public class Java堆栈特性1 {
+    public static void main(String[] args) {
+        Student stu1 = new Student();//建立栈内存，并使用new开辟堆内存的空间
+        Student stu2 = new Student();
+        system.out.println("123");
+        stu1.name="stu1";
+        stu2.name="stu2";
+
+    }
+}
