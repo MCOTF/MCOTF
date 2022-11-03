6dbe41ce0fa9302c39b82d679e6ca93974379db7
MCOTF <2549254469@qq.com>
2022-11-03 20:45:44 +0800

    第二题作业做完力

---
+++ Java课程记录/Java_11_3.java
@@ -0,0 +1,12 @@
+
+public class Java_11_3 {
+    public static void main(String[] args) {
+        int s = 0;
+        int i = 1;
+        while(!(i >4)){
+            s=s+i*i;
+            i++;
+        }
+        System.out.println(s);
+    }
+}
