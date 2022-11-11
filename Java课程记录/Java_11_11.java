28d0b2b6e8c7de83657f35285beac8d0cb00aac9
MCOTF <2549254469@qq.com>
2022-11-11 09:55:09 +0800

    内容越传越离谱

--- Java课程记录/Java_11_11.java
+++ Java课程记录/Java_11_11.java
@@ -1,5 +1,6 @@
 //super调用父类构造方法
 //final关键字
+//abstract抽象类与抽象方法
 class Animal1111{
     private String name;
     private int age;
@@ -30,6 +31,12 @@ final class Final1111{//final关键字
     final int a = 1;//第一次给a赋值后，a被锁死，只能等于1了，final变量不允许更改
 //    a = 2;//再给a赋值会报错
     int b = 1;
+}
+
+abstract class Abstract1111{//抽象类
+    public abstract void abs1();//抽象方法：无方法体，抽象类中必须有一个以上抽象方法>=1，且继承调用的话，那么子类必须实现该抽象类的全部抽象方法
+    public void abs2(){};//普通方法：在抽象类中存在的数量>=0
+    protected abstract void abs3();//抽象方法不能用private修饰，否则会自相矛盾
 }
 public class Java_11_11 {
     public static void main(String[] args) {
