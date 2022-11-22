8db7d3011ffcb56e544576239fbdca617c7a182b
MCOTF <2549254469@qq.com>
2022-11-22 15:17:19 +0800

    test

--- Java课程记录/Java_11_21_43.java
+++ Java课程记录/Java_11_21_43.java
@@ -1,23 +1,23 @@
 //案例4-3 多彩的声音
 //要求：抽象类
-//对象：?，主人，主人以外的人
+//对象：狗，主人，主人以外的人
 abstract class Animal1121 {//抽象类-动物
     //tips：一般抽象类的子类用不到super
     abstract void shut();//动物叫
     abstract void eat();//动物吃
 }
-class Dog1121 extends Animal1121 {//?
+class Dog1121 extends Animal1121 {//狗
     void shut(){//狗正常叫
-        System.out.println("?:汪");
+        System.out.println("狗:汪");
     }
     void shut1(){//狗开心地叫
-        System.out.println("?:汪(开心)");
+        System.out.println("狗:汪(开心)");
     }
     void shut2(){//狗警惕地叫
-        System.out.println("?:汪(警惕)");
+        System.out.println("狗:汪(警惕)");
     }
     void eat(){//狗进食
-        System.out.println("?:正在吃");
+        System.out.println("狗:正在吃");
     }
 }
 abstract class Human1121 {//抽象类-人
