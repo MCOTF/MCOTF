eeb63597b6352c1f4402ae72d7cd4da4b39d51de
MCOTF <2549254469@qq.com>
2022-11-25 10:05:30 +0800

    多态

--- Java课程记录/Java_11_25.java
+++ Java课程记录/Java_11_25.java
@@ -4,9 +4,28 @@
 
 //对象类型转换
 //教师与学生：都属人，仅职位不同（向上转型）
-
+class Animal1125{
+    public void shout(){
+        System.out.println("Animal的shout方法");
+    }
+}
+class Dog1125 extends Animal1125{
+    //重写shout方法
+    public void shout() {
+        System.out.println("汪汪");
+    }
+    public void eat(){
+        System.out.println("吃骨头");
+    }
+}
 public class Java_11_25 {
     public static void main(String[] args) {
-        
+//        Dog dog = new Dog();
+//        Animal an = dog;
+        Animal1125 an = new Dog1125();
+        Dog1125 dog = (Dog1125)an;
+//        an.shout();
+        dog.shout();
+        dog.eat();
     }
 }
