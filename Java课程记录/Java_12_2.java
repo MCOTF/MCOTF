69637b7cfc9ab0bf97b49216602b97dcc07304f5
MCOTF <2549254469@qq.com>
2022-12-06 14:44:06 +0800

    1

--- Java课程记录/Java_12_2.java
+++ Java课程记录/Java_12_2.java
@@ -1,7 +1,12 @@
-
+//Object类
+//内部类-成员内部类，局部内部类，静态内部类，匿名内部类
+class Animal{
+    //默认为object类
+}
 
 public class Java_12_2 {
     public static void main(String[] args) {
-
+        Animal animal = new Animal();
+        System.out.println(animal.toString());
     }
 }
