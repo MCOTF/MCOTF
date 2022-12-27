d1878a9378eab0f278fc44f08b7eb680ba78c1aa
MCOTF <2549254469@qq.com>
2022-12-27 16:23:16 +0800

    呃呃

--- Java课程记录/Java_12_23.java
+++ Java课程记录/Java_12_23.java
@@ -1,6 +1,14 @@
-//string类
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 public class Java_12_23 {
     public static void main(String[] args) {
-
+        DateFormat date =DateFormat.getDateInstance(DateFormat.FULL);
+        String time= date.format(new Date());
+        System.out.println("DateFormat类的FULL格式："+time);
+        //yyyy-MM-dd格式
+        SimpleDateFormat date2=new SimpleDateFormat("yyyy-MM-dd");
+        String time2=date2.format(new Date());
+        System.out.println("SimpleDateFormat类的yyyy-MM-dd格式："+time);
     }
 }
